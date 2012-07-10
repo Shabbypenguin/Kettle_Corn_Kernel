5029708fe5e0d6f61bff5e518066abc69894c369
David Rouleau <Shabbypenguin@gmail.com>
2012-07-10 15:44:48 -0300

    Update master

--- maek.sh
+++ maek.sh
@@ -1,39 +1,55 @@
 #!/bin/bash
 
-    THREADS=$(expr 1 + $(grep processor /proc/cpuinfo | wc -l))
+    THREADS=$(expr 4 + $(grep processor /proc/cpuinfo | wc -l))
     DEFCONFIG=m2_spr_defconfig
     ARCH="ARCH=arm"
-    KERNEL=/home/shabbypenguin/Public/SPH-L710
+    DIR=/home/shabbypenguin/Public
+    RAMDISK=$DIR/Ramdisks/SPH-L710
+    TOOLS=$DIR/Tools
+    KERNEL=$DIR/SPH-L710
     PACK=$KERNEL/package
     OUT=$KERNEL/arch/arm/boot
+    LOG=$TOOLS/log.txt
+
+    # Edit this to change the kernel name
+    KBUILD_BUILD_VERSION="Kettle.Corn-1.0-NO-OC"
+    export KBUILD_BUILD_VERSION
 
     MAKE="make -j${THREADS}"
+    export USE_CCACHE=1
     export $ARCH
 
     # This cleans out crud and makes new config
     $MAKE clean
     $MAKE mrproper
-    rm -rf ./package/ramdisk_tmp/lib
-    rm -f $PACK/zImage
-    rm -f $PACK/ramdisk.gz
-    rm -f $PACK/*oot.*
+    rm -rf $RAMDISK/lib
+    rm -rf $PACK/*
     $MAKE $DEFCONFIG
 
-    # Edit this to change the kernel name
-    KBUILD_BUILD_VERSION="Kettle.Corn-0.0.3-NO-OC"
-    export KBUILD_BUILD_VERSION
+    echo "All clean!" > $LOG
+    date >> $LOG
+    echo "" >> $LOG
 
     # Finally making the kernel
     $MAKE zImage
 
+    echo "Compiled" >> $LOG
+    date >> $LOG
+    echo "" >> $LOG
+
     # This command will detect if the folder is there and if not will recreate it
-    [ -d "$PACK/ramdisk_tmp/lib/modules" ] || mkdir -p "$PACK/ramdisk_tmp/lib/modules"
+    [ -d "$RAMDISK/lib/modules" ] || mkdir -p "$RAMDISK/lib/modules"
 
     # These move files to easier locations
-    find -name '*.ko' -exec cp -av {} $PACK/ramdisk_tmp/lib/modules/ \;
+    find -name '*.ko' -exec cp -av {} $RAMDISK/lib/modules/ \;
     cp $OUT/zImage $PACK
 
     # This part packs the img up :)
     # In order for this part to work you need the mkbootimg tools
-    $PACK/mkbootfs $PACK/ramdisk_tmp | gzip > $PACK/ramdisk.gz
-    $PACK/mkbootimg --cmdline "console=null androidboot.hardware=qcom user_debug=31" --kernel $PACK/zImage --ramdisk $PACK/ramdisk.gz --base 0x80200000 --ramdiskaddr 0x81500000 -o $PACK/boot.img
+
+    $TOOLS/mkbootfs $RAMDISK | gzip > $PACK/ramdisk.gz
+    $TOOLS/mkbootimg --cmdline "console=null androidboot.hardware=qcom user_debug=31" --kernel $PACK/zImage --ramdisk $PACK/ramdisk.gz --base 0x80200000 --ramdiskaddr 0x81500000 -o $PACK/boot.img
+
+    echo "packed" >> $LOG
+    date >> $LOG
+    echo "" >> $LOG
