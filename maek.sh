ae51f866231828dcca800acfdb41e627b1d1994c
David Rouleau <Shabbypenguin@gmail.com>
2012-07-11 14:33:24 -0400

    Fixed overclocking, added cm kernel

--- maek.sh
+++ maek.sh
@@ -5,14 +5,15 @@
     ARCH="ARCH=arm"
     DIR=/home/shabbypenguin/Public
     RAMDISK=$DIR/Ramdisks/SPH-L710
+    CMRAMDISK=$DIR/Ramdisks/SPH-L710-CM
     TOOLS=$DIR/Tools
-    KERNEL=$DIR/SPH-L710
+    KERNEL=$DIR/Kettle_Corn_Kernel
     PACK=$KERNEL/package
     OUT=$KERNEL/arch/arm/boot
     LOG=$TOOLS/log.txt
 
     # Edit this to change the kernel name
-    KBUILD_BUILD_VERSION="Kettle.Corn-1.0-NO-OC"
+    KBUILD_BUILD_VERSION="Kettle.Corn-1.1"
     export KBUILD_BUILD_VERSION
 
     MAKE="make -j${THREADS}"
@@ -32,6 +33,7 @@
 
     # Finally making the kernel
     $MAKE zImage
+    $MAKE modules
 
     echo "Compiled" >> $LOG
     date >> $LOG
@@ -42,6 +44,7 @@
 
     # These move files to easier locations
     find -name '*.ko' -exec cp -av {} $RAMDISK/lib/modules/ \;
+    find -name '*.ko' -exec cp -av {} $CMRAMDISK/lib/modules/ \;
     cp $OUT/zImage $PACK
 
     # This part packs the img up :)
@@ -50,5 +53,7 @@
     $TOOLS/mkbootfs $RAMDISK | gzip > $PACK/ramdisk.gz
     $TOOLS/mkbootimg --cmdline "console=null androidboot.hardware=qcom user_debug=31" --kernel $PACK/zImage --ramdisk $PACK/ramdisk.gz --base 0x80200000 --ramdiskaddr 0x81500000 -o $PACK/boot.img
 
+    $TOOLS/mkbootfs $CMRAMDISK | gzip > $PACK/CMramdisk.gz
+    $TOOLS/mkbootimg --cmdline "console=null androidboot.hardware=qcom user_debug=31" --kernel $PACK/zImage --ramdisk $PACK/CMramdisk.gz --base 0x80200000 --ramdiskaddr 0x81500000 -o $PACK/CM-boot.img
     echo "packed" >> $LOG
     date >> $LOG
